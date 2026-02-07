aa92570a60912b3a5b81b72dd8fcd4b6b0bbbd87
Laila Aziz <lailaaziz@me.com>
2026-02-07 11:59:05 -0800

    add is_verified to users

--- alembic/versions/54373626066e_add_is_verified_to_users.py
+++ alembic/versions/54373626066e_add_is_verified_to_users.py
@@ -1,30 +1,11 @@
-"""add is_verified to users
+"""add is_verified to users"""
 
-Revision ID: 54373626066e
-Revises: 1fe376ef9bb4
-Create Date: 2026-02-07 10:00:24.191118
-
-"""
-
-from alembic import op
-import sqlalchemy as sa
-
-revision = '54373626066e'
-down_revision = '1fe376ef9bb4'
-branch_labels = None
-depends_on = None
-
-def upgrade() -> None:
-    pass
-
-def downgrade() -> None:
-    pass
 from alembic import op
 import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
-revision = "NEW_ID"
-down_revision = "1fe376ef9bb4"  # <-- your current latest revision
+revision = "54373626066e"
+down_revision = "1fe376ef9bb4"
 branch_labels = None
 depends_on = None
 
@@ -32,7 +13,12 @@ depends_on = None
 def upgrade():
     op.add_column(
         "users",
-        sa.Column("is_verified", sa.Boolean(), nullable=False, server_default=sa.false()),
+        sa.Column(
+            "is_verified",
+            sa.Boolean(),
+            nullable=False,
+            server_default=sa.false(),
+        ),
     )
 
 
